a01c2bc8816fab9d61d7b5a23e62ad108ae0fc54
KarenCortes <karenj.cortesn@gmail.com>
2024-02-07 22:16:55 -0500

    angular material

---
+++ angular-material/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { AppComponent } from './app.component';
+import { CardsComponent } from './cards/cards.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardsComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
